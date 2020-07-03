2b5ca2fb2fa5b97ccc342d3e018a9859ebfe2e77
Tony <elkobrat@protonmail.com>
2020-07-03 14:42:04 +1000

    prepared webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,67 @@
-const webpack = require('webpack');
 const path = require('path');
-const cssnano = require('cssnano');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
+const webpack = require('webpack');
+const cssnano = require('cssnano');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-
 
 module.exports = {
   entry: {
     main: './src/index.js',
-    secondary: './src/saved/index.js',
+    articles: './src/articles/index.js',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[chunkhash].js',
+    filename: '[name]/[name].[chunkhash].js',
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      use: { loader: 'babel-loader' },
-      exclude: /node_modules/,
-    },
-    {
-      test: /\.css$/i,
-      use: [
-        { loader: MiniCssExtractPlugin.loader, options: { publicPath: '../' } },
-        'css-loader',
-        'postcss-loader',
-      ],
-    },
-    {
-      test: /\.(png|jpg|gif|ico|svg)$/,
-      use: [
-        'file-loader?name=./images/[name].[ext]',
-        {
-          loader: 'image-webpack-loader',
-          options: {
-            mozjpeg: {
-              progressive: true,
-              quality: 65,
-            },
-            optipng: {
-              enabled: false,
-            },
-            pngquant: {
-              quality: [0.65, 0.9],
-              speed: 4,
-            },
-            gifsicle: {
-              interlaced: false,
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          { loader: MiniCssExtractPlugin.loader, options: { publicPath: '../' } },
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+
+      {
+        test: /\.(png|jpg|gif|ico|svg)$/,
+        use: [
+          'file-loader?name=./images/[name].[ext]',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              },
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
             },
           },
-        },
-      ],
-    },
-    {
-      test: /\.(eot|ttf|woff|woff2)$/,
-      loader: 'file-loader?name=./vendor/[name].[ext]',
-    },
-
+        ],
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=./vendor/fonts/[name].[ext]&publicPath=../',
+      },
     ],
   },
   plugins: [
@@ -81,16 +83,20 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       inject: false,
-      template: './src/saved/index.html',
-      filename: './saved/index.html',
+      template: './src/articles/index.html',
+      filename: './articles/index.html',
     }),
     new WebpackMd5Hash(),
     new webpack.DefinePlugin({
       NODE_ENV: JSON.stringify(process.env.NODE_ENV),
     }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: cssnano,
+      cssProcessorPluginOptions: {
+        preset: ['default'],
+      },
+      canPrint: true,
+    }),
   ],
-
-  devServer: {
-    contentBase: path.join(__dirname, './dist'),
-  },
 };
